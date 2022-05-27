86a4aab67f36706d0c6e421e5e739663b245af9f
bhansaliyash <bhansaliyashg@gmail.com>
2022-05-26 20:37:13 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import * as React from "react";
+import { useEffect } from "react";
 import { ethers } from "ethers";
 import './App.css';
 
-export default function App() {
+const App = () => {
+  const checkIfWalletIsConnected = () => {
+    const {ethereum} = window;
 
-  const wave = () => {
-    
+    if (!ethereum) {
+      console.log("Not logged");
+    }
+    else{
+      console.log("Connected ", ethereum);
+    }
   }
-  
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
     <div className="mainContainer">
-
       <div className="dataContainer">
         <div className="header">
         👋 Hey there!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
       </div>
     </div>
   );
+
 }
+
+export default App
+
